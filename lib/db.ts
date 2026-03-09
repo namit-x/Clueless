70200ebafe25a573d3ffd9191ec31b53ab40598c
Namit <rnamit099@gmail.com>
2026-03-10 00:34:51 +0530

    updated pool configs

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,12 @@
 import { Pool } from "pg";
 
 export const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "ClueLess",
-    password: "StoreBase@1212",
+    user: "postgres.mawalvwezsqulvzmxdck",
+    host: "aws-1-ap-southeast-2.pooler.supabase.com",
+    database: "postgres",
+    password: "Clueless!@_JAIN",
     port: 5432,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
