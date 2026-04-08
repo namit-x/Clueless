bac1613998bdc49b9a0eb30d534bb195285f1fde
Namit <rnamit099@gmail.com>
2026-04-08 18:59:07 +0530

    resolved remaining merge conflicts

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,10 +1,6 @@
 "use client";
 
-<<<<<<< HEAD
 import { useState, useEffect } from "react";
-=======
-import { useState } from "react";
->>>>>>> dev
 import HeroSection from "@/components/HeroSection";
 import PrizesSection from "@/components/PrizesSection";
 import Timeline from "@/components/Timeline";
